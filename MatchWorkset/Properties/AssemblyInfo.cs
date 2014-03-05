f7c0d2a350236b67ac15c9a12f0cd608b4fd84a1
Doug Klassen <doug_klassen@hotmail.com>
2014-03-04 20:33:50 -0800

    Version 0.9 - works correctly in straightforward situations

--- MatchWorkset/Properties/AssemblyInfo.cs
+++ MatchWorkset/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NewAddIn")]
-[assembly: AssemblyDescription("A Revit 2014 Add-In")]
+[assembly: AssemblyTitle("MatchWorkset")]
+[assembly: AssemblyDescription("A tool for assigning elements to worksets")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -21,7 +21,7 @@ using System.Resources;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("59acf2a6-14a7-46a4-8bff-5eb42ae3282f")]
+[assembly: Guid("330d3e46-6ecd-4194-9ff4-bea3d9932c9b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("14.0.0.0")]
-[assembly: AssemblyFileVersion("14.0.0.0")]
+[assembly: AssemblyVersion("14.0.9.0")]
+[assembly: AssemblyFileVersion("14.0.9.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
